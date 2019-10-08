b4ac9f2a0d273b9f286f5949a856eab4fe896957
pedroid <ntuyusheng@gmail.com>
2019-10-08 23:29:38 +0800

    20191008

--- js/mdpp.js
+++ js/mdpp.js
@@ -6,8 +6,7 @@ function mdppManager(){
 
 mdppManager.prototype.init = function(){
 	this.curr_mdpp_state_id = 0;
-	this.reg_mdpp_statements = [];
-	this.list_mdpp_states = [];	
+	this.list_mdpp_cmd_reg = [];
 	this.variable_manager = new VariableManager; 
 	this.dict_md_ext_cmd = {
 		"html_block_start": {
@@ -21,16 +20,16 @@ mdppManager.prototype.init = function(){
 		}
 		
 	};
-	this.dict_mdpp_class = {
-		"md_null":0,//markdown
-		"md_ext":1,//markdown-extension
-		"md":2
+	this.dict_mdpp_states = {
+		"idle":0,
+		"block_html":1,
 	}
+
 }
 mdppManager.prototype.clear = function(){
-	this.curr_mdpp_state_id = this.dict_mdpp_class['md_null'];
+	this.curr_mdpp_state_id = this.dict_mdpp_states['idle'];
 	this.reg_mdpp_statements = [];
-	this.list_mdpp_states = [];		
+	this.list_mdpp_cmd_reg = [];		
 	this.variable_manager.clearVariables();
 }
 var mdppObject = function(mdpp_cmd, mdpp_arguments) {
@@ -50,62 +49,82 @@ mdppManager.prototype.mdpp2ListDiv = function (input_content) {
 
 }
 
-mdppManager.prototype.exec_mdpp_cmd = function(next_mdpp_class_id, mdpp_input){
+mdppManager.prototype.exec_mdpp_cmd = function(mdpp_input){
 	//console.log('exec_mdpp_cmd:'+next_mdpp_class_id+','+next_mdpp_type_id+','+mdpp_input);
-	console.log('exec_mdpp_cmd:'+next_mdpp_class_id+','+mdpp_input);
 	console.log('curr_state:'+this.curr_mdpp_state_id);
-	console.log('null_state:'+this.dict_mdpp_class['md_null']);
-	if (this.curr_mdpp_state_id == this.dict_mdpp_class['md_null']){
-		switch(next_mdpp_class_id){
-			case this.dict_mdpp_class['md']:
-				this.curr_mdpp_state_id = this.dict_mdpp_class['md'];		
-				break;
-			case this.dict_mdpp_class['md_ext']:
-				this.curr_mdpp_state_id = this.dict_mdpp_class['md_ext'];				
-				break;
-			default:
-				this.curr_mdpp_state_id = this.dict_mdpp_class['md_null'];
-		}		
-	}else if (this.curr_mdpp_state_id == this.dict_mdpp_class['md']){
-		switch(next_mdpp_class_id){
-			case this.dict_mdpp_class['md_null']:
-				this.curr_mdpp_state_id = this.dict_mdpp_class['md_null'];		
-				break;
-			case this.dict_mdpp_class['md_ext']:
-				this.curr_mdpp_state_id = this.dict_mdpp_class['md_ext'];				
-				break;
-		}		
+	console.log('exec_mdpp_cmd:'+mdpp_input);	
+	
+	if (this.curr_mdpp_state_id == this.dict_mdpp_states['idle']){		
+		
+		if(this.dict_md_ext_cmd["html_block_start"].pattern.test(mdpp_input)){	
+			this.curr_mdpp_state_id = this.dict_mdpp_states["block_html"];
+		}else{
+			this.list_mdpp_cmd_reg.push(
+				{
+					'type':'md',
+					'cmd':mdpp_input
+				}
+			);
+		}
+		
+
+	}else if(this.curr_mdpp_state_id == this.dict_mdpp_states['block_html']){
+		if(this.dict_md_ext_cmd["end"].pattern.test(mdpp_input)){	
+				this.curr_mdpp_state_id = this.dict_mdpp_states["idle"];			
+		}else{
+			this.list_mdpp_cmd_reg.push(
+				{
+					'type':'html',
+					'cmd':mdpp_input
+				}
+			);			
+		}
 	}
 	
 	
 	
 }
+function mdpp_cmd_Object(mdpp_cmd_type, mdpp_cmd_class, mdpp_cmd_arguments, mdpp_cmd_content){
+	this.mdpp_cmd_type = mdpp_cmd_type;
+	this.mdpp_cmd_class = mdpp_cmd_class;
+	this.mdpp_cmd_arguments = mdpp_cmd_arguments;
+	this.mdpp_cmd_content = mdpp_cmd_content;
+}
+
+mdppManager.prototype.mdpp_cmd_classifier = function(str_mdpp_cmd){
+	let mdpp_type = "md";
+	let mdpp_class = "md";
+	let mdpp_arguments = "";
+	let mdpp_content = "";
+
+	for ( each_md_ext_cmd in this.dict_md_ext_cmd) {
+		//cmd match md_ext pattern
+		if(this.dict_md_ext_cmd[each_md_ext_cmd].pattern.test(str_mdpp_cmd)){			
+			switch(each_md_ext_cmd){
+				default:
+					mdpp_class = "md_ext";
+					mdpp_type = each_md_ext_cmd;
+					mdpp_arguments = "";
+					mdpp_content = str_mdpp_cmd;
+			}
+		}		
+	}
+	let mdpp_cmd_object = new mdpp_cmd_Object(mdpp_type, mdpp_class, mdpp_arguments, mdpp_content);
+	return mdpp_cmd_object;
+}
+
 mdppManager.prototype.str_mdpp2list_mdpp_object= function (str_mdpp) {	
 	console.log("debug start");
 	tmp = [];
 	this.clear();
 	var list_mdpp_object = [];
-	list_mdpp_cmds = str_mdpp.split('\n');
-	var next_mdpp_class_default = this.dict_mdpp_class["md_null"];
-	var next_mdpp_class_id = next_mdpp_class_default;
+	list_input_mdpp_cmds = str_mdpp.split('\n');
 	var cnt = 0;
-	for( each_mdpp_cmd_id in list_mdpp_cmds){
-		next_mdpp_class_id = this.dict_mdpp_class["md"];
-		//next_mdpp_type_id = this.dict_mdpp_type["md_null"];
-		console.log('curr mdpp state:'+this.curr_mdpp_state_id);
-		console.log('input[' + each_mdpp_cmd_id +']:'+ list_mdpp_cmds[each_mdpp_cmd_id]);
+	for( each_mdpp_cmd_id in list_input_mdpp_cmds){		
+		//console.log('curr mdpp state:'+this.curr_mdpp_state_id);
+		//console.log('input[' + each_mdpp_cmd_id +']:'+ list_input_mdpp_cmds[each_mdpp_cmd_id]);
 		
-
-		for ( each_md_ext_cmd in this.dict_md_ext_cmd) {				
-			if ( this.dict_md_ext_cmd[each_md_ext_cmd].pattern.test(list_mdpp_cmds[each_mdpp_cmd_id])) {									
-				next_mdpp_class_id = this.dict_mdpp_class['md_ext'];		
-				//next_mdpp_type_id = each_md_ext_cmd;
-				break;
-			}
-			
-		}
-		
-		this.exec_mdpp_cmd(next_mdpp_class_id, list_mdpp_cmds[each_mdpp_cmd_id]);
+		this.exec_mdpp_cmd(list_input_mdpp_cmds[each_mdpp_cmd_id]);
 		
 
 	}
